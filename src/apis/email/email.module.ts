b5d4fc48f624e47cb4fd6ce9f146aeb388457cfd
Thomas <thomaslbp19@gmail.com>
2026-02-16 15:16:56 -0300

    refactor: update email module to include template service
    - add TemplateService to providers and exports in EmailModule

--- src/apis/email/email.module.ts
+++ src/apis/email/email.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { TemplateService } from './template.service';
 
 @Module({
   controllers: [],
-  providers: [EmailService],
-  exports: [EmailService],
+  providers: [EmailService, TemplateService],
+  exports: [EmailService, TemplateService],
 })
 export class EmailModule {}
